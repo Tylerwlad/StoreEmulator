58227101cf02ace5008cba871b4d082f6a0f52f9
Влад <Tylerwlad@mail.ru>
2023-04-24 12:30:52 +0700

    add method loadManager, in the main method, replacing the class with an interface

--- src/TradeManager/FileBackedTradeManager.java
+++ src/TradeManager/FileBackedTradeManager.java
@@ -1,6 +1,11 @@
 package TradeManager;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FileBackedTradeManager implements TradeManager {
@@ -12,7 +17,26 @@ public class FileBackedTradeManager implements TradeManager {
 
     @Override
     public void loadManager() {
-
+        Reader fileReader;
+        ArrayList<String> fileInArrayList = new ArrayList<>();
+        try {
+            fileReader = new FileReader("src\\WareHouse.csv");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedReader bf = new BufferedReader(fileReader)) {
+            while (bf.ready()) {
+                fileInArrayList.add(bf.readLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Файл не найден");
+        }
+        for (String s: fileInArrayList) {
+            String[] a = s.split(", ");
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i]);
+            }
+        }
     }
 
     @Override
